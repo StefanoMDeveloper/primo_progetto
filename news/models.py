b8700745e7ba43230340b935e26730186034c83a
Zst3el <50278017+Zst3el@users.noreply.github.com>
2020-12-02 16:14:17 +0100

    manca ultimo pezzo

--- news/models.py
+++ news/models.py
@@ -4,7 +4,7 @@ class Giornalista(models.Model):
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
 
-    def str(self):
+    def __str__(self):
         return self.nome + " " + self.cognome
 
 class Articolo (models.Model):
@@ -12,5 +12,5 @@ class Articolo (models.Model):
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
 
-    def str(self):
+    def __str__(self):
         return self.titolo
--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, ArticoloDetailViewCB, ArticoloListView
 
 app_name = 'news'
 
 urlpatterns = [
-    path('', home, name="homeview"),
+    path("", home, name="homeview"),
+    path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail"),
+    path("lista_articoli/", ArticoloListView.as_view(), name="lista_articoli"),
+    path("lista_articoli/", ArticoloListView.as_view(), name="lista_")
 ]
