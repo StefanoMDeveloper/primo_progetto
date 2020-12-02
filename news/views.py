698991ca693ea5dcbc334075e7d1d0e33f5cf625
Zst3el <50278017+Zst3el@users.noreply.github.com>
2020-12-02 12:51:04 +0100

    modifiche varie

--- news/views.py
+++ news/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Articolo,Giornalista
 # Create your views here.
 
 
 def home(request):
-    a = ""
-    g = ""
-    for art in Articolo.objects.all():
-        a += (art.titolo + "<br>")
-
-    for gio in Giornalista.object.all():
-        g += (gio.nome + "<br>")
-    response = "Articoli:<br>" + a + "<br>Giornalisti:<br>" + g
-
-    return HttpResponse("<h1>Homepage news!</h1>")
+    articoli = Articolo.objects.all()
+    giornalisti = Giornalista.objects.all()
+    context={"articoli": articoli, "giornalisti":giornalisti}
+    print(context)
+    return render(request, "home.html", context)
