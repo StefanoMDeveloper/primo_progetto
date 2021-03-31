ef4d71440b3b8adb7af7aaa2cff66f30284d7077
Zst3el <50278017+Zst3el@users.noreply.github.com>
2021-03-31 15:25:25 +0200

    Contatore

--- prima_app/views.py
+++ prima_app/views.py
@@ -26,4 +26,11 @@ def variabili(request):
 
 
 def index(request):
-    return render(request, "index.html")
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    context = {
+        "num_visits":num_visits,
+    }
+
+    return render(request, "index.html", context)
+    
